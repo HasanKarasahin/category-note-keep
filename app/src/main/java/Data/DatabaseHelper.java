ea342c7a388dba0a17d9858c676d9a8199a4aa66
HasanGaziKarasahin <hasangazikarasahin@gmail.com>
2018-11-29 17:52:50 +0300

    DatabaseHelper create tablo sorguları eklendi

--- app/src/main/java/Data/DatabaseHelper.java
+++ app/src/main/java/Data/DatabaseHelper.java
@@ -5,13 +5,33 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
+import Data.DatabaseContract.NotlarEntry;
+import Data.DatabaseContract.KategorilerEntry;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "database.db";
     private static final int DATABASE_VERSION = 1;
 
-    private static final String TABLE_KATEGORILER_CREATE = "";
-    private static final String TABLE_NOTLAR_CREATE = "";
+    //CREATE TABLE KATEGORILER(_ID INTEGER PRIMARY KEY, KATEGORILER TEXT);
+
+    private static final String TABLE_KATEGORILER_CREATE = "CREATE TABLE " + KategorilerEntry.TABLE_NAME +
+            " (" + KategorilerEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KategorilerEntry.COLUMN_KATEGORI + " TEXT )";
+
+    //CREATE TABLE NOTLAR (_ID INTEGER PRIMARY KEY,
+    //                    NOT_ICERIK TEXT,
+    //                    OLUSTURMA_TARIHI TEXT default CURRENT_TIMESTAMP,
+    //                    BITIS_TARIHI TEXT
+    //                    KATEGORI_ID INTEGER
+    //                    FOREIGN KEY(KATEGORI_ID) REFERENCES KATEGORILER(_ID);
+
+    private static final String TABLE_NOTLAR_CREATE = "CREATE TABLE " + NotlarEntry.TABLE_NAME + " (" + NotlarEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            NotlarEntry.COLUMN_NOT_ICERIK + " TEXT, " +
+            NotlarEntry.COLUMN_OLUSTURULMA_TARIHI + " TEXT default CURRENT_TIMESTAMP, " +
+            NotlarEntry.COLUMN_BITIS_TARIHI + " TEXT, " +
+            NotlarEntry.COLUMN_YAPILDI + " INTEGER, " +
+            NotlarEntry.COLUMN_KATEGORI_ID + " INTEGER, " +
+            "FOREIGN KEY(" + NotlarEntry.COLUMN_KATEGORI_ID + ") REFERENCES " + KategorilerEntry.TABLE_NAME + "(" + KategorilerEntry._ID + ") )";
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -19,19 +39,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //TABLOLARIMIZ BURADA OLUSTURULUYOR.
         db.execSQL(TABLE_KATEGORILER_CREATE);
         db.execSQL(TABLE_NOTLAR_CREATE);
     }
 
     @Override
     public void onConfigure(SQLiteDatabase db) {
+        //FOREIGN KEYLERIN DÜZGÜN CALISMASI ICIN GEREKLI CONFIG AYARLANMASI
         db.setForeignKeyConstraintsEnabled(true);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("");
-        db.execSQL("");
+        //BURAYA YEDEK ALMA ISLEMLERI EKLENECEKTIR.
+        db.execSQL("DROP TABLE IF EXISTS " + KategorilerEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + NotlarEntry.TABLE_NAME);
         onCreate(db);
     }
 }
