fd48641e33857a37b02ff2a88fb47f54462816cf
HasanGaziKarasahin <hasangazikarasahin@gmail.com>
2018-11-29 15:28:32 +0300

    DatabaseHelper update

--- app/src/main/java/Data/DatabaseHelper.java
+++ app/src/main/java/Data/DatabaseHelper.java
@@ -6,17 +6,32 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    public static final String DATABASE_NAME = "database.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_KATEGORILER_CREATE = "";
+    private static final String TABLE_NOTLAR_CREATE = "";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_KATEGORILER_CREATE);
+        db.execSQL(TABLE_NOTLAR_CREATE);
+    }
 
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        db.setForeignKeyConstraintsEnabled(true);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("");
+        db.execSQL("");
+        onCreate(db);
     }
 }
