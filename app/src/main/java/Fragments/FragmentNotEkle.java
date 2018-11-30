3e6b0e202536eaa1ebb9130b03a7ff056428ef60
HasanGaziKarasahin <hasangazikarasahin@gmail.com>
2018-11-30 15:55:43 +0300

    FragmentNotEkle spinnerdan secilen kategoriye not ekleniyor

--- app/src/main/java/Fragments/FragmentNotEkle.java
+++ app/src/main/java/Fragments/FragmentNotEkle.java
@@ -1,6 +1,7 @@
 package Fragments;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,13 +9,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.hasan.category_note_keep.R;
 
+import java.util.ArrayList;
+
+import Model.KategoriModel;
+import data.DatabaseResult;
+
 public class FragmentNotEkle extends Fragment {
 
+    DatabaseResult dbResult;
+    Spinner spKategoriler;
+    ArrayList<KategoriModel> kategoriModels;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,18 +40,42 @@ public class FragmentNotEkle extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        dbResult = new DatabaseResult(getContext());
+
+        spKategoriler = view.findViewById(R.id.spKategoriler);
+        spinnerDoldur();
         Button btn = view.findViewById(R.id.btnNotEkle);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String kategoriId = "1";
-
-                //notEkle(KategoriId);
+                String kategoriId = ((KategoriModel) spKategoriler.getSelectedItem()).get_id();
+                String notIcerik = ((EditText) (view.findViewById(R.id.etNotIcerik))).getText().toString();
+                notEkle(notIcerik, kategoriId);
             }
         });
     }
+
+
+    private void notEkle(String notIcerik, String kategoriId) {
+        Uri uri = dbResult.setNotEkle(notIcerik, kategoriId);
+        if (uri != null) {
+
+            Toast.makeText(getContext(), "Not Basarıyla Eklendi", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), "Not Eklenemedi", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void spinnerDoldur() {
+        kategoriModels = dbResult.getTumKategoriler();
+        ArrayAdapter<KategoriModel> adapter = new ArrayAdapter<KategoriModel>(getContext(), R.layout.support_simple_spinner_dropdown_item, kategoriModels);
+        spKategoriler.setAdapter(adapter);
+        spKategoriler.setSelection(0);
+    }
+
 
     @Override
     public void onAttach(Context context) {
