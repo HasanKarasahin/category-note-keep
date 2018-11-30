d9c970e6e94af94aa64be17dfd15ae5794fa3ee4
HasanGaziKarasahin <hasangazikarasahin@gmail.com>
2018-11-30 10:00:58 +0300

    FragmentNotEkle butonuna click eventi atandı.Not ekle fonksiyonu olusturuldu.

--- app/src/main/java/Fragments/FragmentNotEkle.java
+++ app/src/main/java/Fragments/FragmentNotEkle.java
@@ -2,14 +2,19 @@ package Fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.hasan.category_note_keep.R;
 
 public class FragmentNotEkle extends Fragment {
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,20 @@ public class FragmentNotEkle extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_fragment_not_ekle, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btn = view.findViewById(R.id.btnNotEkle);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String kategoriId = "1";
+
+                notEkle(KategoriId);
+            }
+        });
     }
 
     @Override
